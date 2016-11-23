9430a9f54fa743745a520bb43c0edce14586d90c
Stefan Fiel <stefan@nomacs.org>
2016-11-23 09:08:36 +0100

    deleted files which are not needed in this fork

--- src/main/java/eu/transkribus/interfaces/example_plugin/package-info.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * Example implementation for a module in plugin fashion
- * Modules should implement:
- * 	1.) The corresponding interface in the eu.transkribus.interfaces package
- *  2.) The ModuleFactory class in the eu.transkribus.interfaces package
- */
-package eu.transkribus.interfaces.example_plugin;
